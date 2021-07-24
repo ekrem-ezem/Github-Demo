654dabaa499c183d8d7c4f7df3018bea39c51c79
Ekrem <85905447+ekrem-ezem@users.noreply.github.com>
2021-07-24 04:01:18 +0200

    Update Getrepos.js

--- src/components/Getrepos.js
+++ src/components/Getrepos.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Getrepos() {
-  const [userList, setUseList] = useState([]);
+  const [reposts, getList] = useState([]);
 
   useEffect(() => {
     axios
@@ -10,7 +10,7 @@ function Getrepos() {
         "http://api.github.com/users/coryhouse/repos?client_id=757e310c1b402287bab5&client_secret=43619d7e29ca00fd0800178405fd4fdae183b169&sort=created"
       )
       .then((res) => {
-        setUseList(res.data);
+        getList(res.data);
         console.log(res.data);
       });
   }, []);
@@ -19,8 +19,8 @@ function Getrepos() {
     <div className="Reposterie-use">
       <h1>User Repositoris</h1>
       <ul className="List-name">
-        {userList &&
-          userList.map((x) => (
+        {reposts &&
+          reposts.map((x) => (
             <li>
               <a href={x.html_url}>{x.name}:</a>
               {x.description}
