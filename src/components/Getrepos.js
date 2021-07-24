13ff97e9518277ba06a0fe14f0b1fc0ce75f9456
Ekrem <85905447+ekrem-ezem@users.noreply.github.com>
2021-07-24 03:14:28 +0200

    Update Getrepos.js

--- src/components/Getrepos.js
+++ src/components/Getrepos.js
@@ -1,42 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { data } from "browserslist";
 
-export default function Getrepos() {
-  const [useList, setUseList] = useState([]);
+function Getrepos() {
+  const [userList, setUseList] = useState([]);
 
   useEffect(() => {
     axios
       .get(
         "http://api.github.com/users/coryhouse/repos?client_id=757e310c1b402287bab5&client_secret=43619d7e29ca00fd0800178405fd4fdae183b169&sort=created"
       )
-      .then((response) => {
-        setUseList(response.data.edge);
-        console.log(response.data);
+      .then((res) => {
+        setUseList(res.data);
+        console.log(res.data);
       });
   }, []);
+
   return (
-    <div>
+    <div className="Reposterie-use">
       <h1>User Repositoris</h1>
-      <ul className="user-repos">
-        <li>
-          <span>reactapp:</span>
-          {data.react}
-        </li>
-        <li>
-          <span>testing-react:</span>
-          {data.name}
-        </li>
-        <li>
-          <span>hallmark:</span>
-        </li>
-        <li>
-          <span>react-testing-libray:</span>
-        </li>
-        <li>
-          <span>auth0-recat-samples:</span>
-        </li>
+      <ul className="List-name">
+        {userList &&
+          userList.map((x) => (
+            <li>
+              <a href={x.html_url}>{x.name}:</a>
+              {x.description}
+            </li>
+          ))}
       </ul>
     </div>
   );
 }
+export default Getrepos;
